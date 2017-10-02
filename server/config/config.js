78b2f9f736b9e21864ecfc484d75a9ced890ad7e
fan <yangfcm@outlook.com>
2017-10-02 22:55:05 +1030

    Config environment

---
+++ server/config/config.js
@@ -0,0 +1,13 @@
+var env = process.env.NODE_ENV || 'development';	
+//This variable is only set by Heroku, not locally
+
+//console.log('env *******', env);
+
+if(env === 'development') {
+	process.env.PORT = 3000;
+	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if(env === 'test') {
+	process.env.PORT = 3000;
+	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
+
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
 	task: "First test todo"
 }, {
 	_id: new ObjectID(),
-	task: "Second test todo"
+	task: "Second test todo",
+	completed: true,
+	completedAt: 1000
 }];
 
 beforeEach((done) => {
@@ -135,6 +137,44 @@ describe('DELETE /todos/:id', () => {
 		request(app)
 			.delete('/todos/invalid_id_123')
 			.expect(404)
+			.end(done);
+	});
+});
+
+describe('PATCH /todos/:id', () => {
+	it('should update the todo', (done) => {
+		var hexId = todos[0]._id.toHexString();
+		var task = 'This should be the new task';
+		request(app)
+			.patch(`/todos/${hexId}`)
+			.send({
+				completed: true,
+				task: task
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.task).toBe(task);
+				expect(res.body.todo.completed).toBe(true);
+				expect(res.body.todo.completedAt).toBeA('number');
+			})
+			.end(done);
+	});
+
+	it('should clear completedAt when todo is not completed', (done) => {
+		var hexId = todos[1]._id.toHexString();
+		var task = 'This should be another new task';
+		request(app)
+			.patch(`/todos/${hexId}`)
+			.send({
+				completed: false,
+				task: task
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.task).toBe(task);
+				expect(res.body.todo.completed).toBe(false);
+				expect(res.body.todo.completedAt).toNotExist();
+			})
 			.end(done);
 	});
 
